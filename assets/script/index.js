380bddb869811cc8e18ba6eb69a2e6e838fd5ae0
Henry <hbreukelman20@gmail.com>
2024-04-04 13:53:11 -0500

    eventlisterners and selectors

--- assets/script/index.js
+++ assets/script/index.js
@@ -4,15 +4,39 @@
   selectors
 */
 
-
+const accept = document.querySelector('.accept');
+const settings = document.querySelector('.settings');
+const save = document.querySelector('.save');
+const browser = document.querySelector('.browser');
+const operatingSysten = document.querySelector('.operatingSysten');
+const screenWidth = document.querySelector('.screenWidth');
+const screenHeight = document.querySelector('.screenHeight');
 
 /*
   functions
 */
 
+function openDialog() {
+  
+}
 
+function acceptAll() {
+
+}
+
+function openSettings() {
+
+}
+
+function saveSettings() {
+
+}
 
 /*
   eventlisteners
 */
 
+accept.addEventListener('click', acceptAll);
+settings.addEventListener('click', openSettings);
+save.addEventListener('click', saveSettings);
+window.addEventListener('load', openDialog);
